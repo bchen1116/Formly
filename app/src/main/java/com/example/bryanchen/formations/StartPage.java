194ec78198547949ae0eabd02420aaa8ce01c479
Bryan Chen <bchen1116@gmail.com>
2017-12-03 16:28:35 -0500

    logo, frontpage working progress

--- app/src/main/java/com/example/bryanchen/formations/StartPage.java
+++ app/src/main/java/com/example/bryanchen/formations/StartPage.java
@@ -3,26 +3,18 @@ package com.example.bryanchen.formations;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class StartPage extends AppCompatActivity {
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    public int numLoadedFrags = 0;
-    private int FORMATIONRESULT = 15;
     private int GET_MAIN_REQUEST = 15;
     private ArrayList<MainActivity> mains = new ArrayList<>();
 
@@ -31,44 +23,6 @@ public class StartPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
 
-        db.collection("User1").document("Num Frags")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                Log.d("LOAD", "DocumentSnapshot data: Number of Fragments: " + task.getResult().getData());
-                                Long loadedNum = (Long) document.getData().get("num");
-
-                                Thread t = new Thread() {
-                                    public void run() {
-                                        updateNumFrags(loadedNum.intValue());
-                                    }
-                                };
-
-                                t.start();
-                                try
-                                {
-                                    t.join();
-                                }catch(InterruptedException ie)
-                                {
-                                    //Log message if required.
-                                }
-                            } else {
-                                Log.d("ERROR", "No such document");
-                            }
-                        } else {
-                            Log.d("FAIL", "get failed with ", task.getException());
-                        }
-                    }
-                });
-
-        TextView startText = (TextView) findViewById(R.id.titlePage);
-        startText.setText("FormLy");
-        startText.setTextSize(60);
-        startText.setGravity(Gravity.CENTER);
 //        TextView startText = (TextView) findViewById(R.id.titlePage);
 //        startText.setText("FormLy");
 //        startText.setTextSize(60);
@@ -87,32 +41,25 @@ public class StartPage extends AppCompatActivity {
                 Toast.makeText(StartPage.this, "Creating new formations", Toast.LENGTH_SHORT).show();
                 Intent creatingIntent = new Intent(view.getContext(), MainActivity.class);
 //                startActivityForResult(creatingIntent, FORMATIONRESULT);
-
-//                Bundle bundle = new Bundle();
-//                bundle.putInt("numFrags", numLoadedFrags);
-//                creatingIntent.putExtras(bundle);
-                creatingIntent.putExtra("numFrags", numLoadedFrags);
-                startActivity(creatingIntent);
                 startActivityForResult(creatingIntent, GET_MAIN_REQUEST);
             }
         });
     }
 
-//    public void onActivityResult (int requestCode, int resultCode, Intent data) {
-//        if (requestCode == FORMATIONRESULT) {
-//            if (resultCode == RESULT_OK) {
-//                mains.add(data.getParcelableArrayListExtra("DOTS"));
-//                Slidescreen f = (Slidescreen)myAdapter.getCurrentFrag(mViewPager.getCurrentItem());
-//                f.setDots(dots);
-//                for (Fragment fr:  myAdapter.getFragment()) {
-//                    Slidescreen frag = (Slidescreen) fr;
-//                    frag.updateDots(dots);
-//                }
-//                myAdapter.notifyDataSetChanged();
-//
-//            }
-//
-//        }
-//    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_MAIN_REQUEST && resultCode == RESULT_OK) {
+            Log.e("the result OK", data.toString());
+            FragList f = data.getExtras().getParcelable("FINAL");
+//            Log.e("NULL SHIT", String.valueOf(data.getExtras().getParcelable("FINAL") != null));
+            Log.e("bundlestart page", "" + f.toString());
+//                Log.e("startPageFragsB", b.describeContents()+"");
+//                Log.e("Number of pages", b.getParcelable("FINAL").toString());
+
+//                f = data.getParcelableExtra("FINAL");
+//                Log.e("startPageFrags", data.getParcelableExtra("FINAL").describeContents()+"");
+        }
+
+    }
 
 }
